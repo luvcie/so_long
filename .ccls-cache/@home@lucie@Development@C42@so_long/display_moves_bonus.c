cf756f3ca6bbfeb1745c9c39d30055a26f19e0a1
Luvcie <lucpardo@student.42.fr>
2025-09-30 19:46:12 +0200

    fix counter text overlap

--- .ccls-cache/@home@lucie@Development@C42@so_long/display_moves_bonus.c
+++ .ccls-cache/@home@lucie@Development@C42@so_long/display_moves_bonus.c
@@ -6,20 +6,40 @@
 /*   By: lucpardo <lucpardo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 18:49:41 by lucpardo          #+#    #+#             */
-/*   Updated: 2025/09/30 18:49:54 by lucpardo         ###   ########.fr       */
+/*   Updated: 2025/09/30 19:44:02 by lucpardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "so_long.h"
 
+// draws black pixels in a 150x30 pixels rectangle to clear old text
+static void	clear_text_area(t_game *game)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (y < 30)
+	{
+		x = 0;
+		while (x < 150)
+		{
+			mlx_pixel_put(game->mlx, game->win, x, y, 0x000000);
+			x++;
+		}
+		y++;
+	}
+}
+
+// converts int move count to a string, displays it on screen at coordinates 10,20
 void	display_moves_on_screen(t_game *game)
 {
 	char	*moves_str;
 	char	*display_text;
 
+	clear_text_area(game);
 	moves_str = ft_itoa(game->player.moves);
 	display_text = ft_strjoin("Moves: ", moves_str);
-	mlx_string_put(game->mlx, game->win, 10, 20, 0xFFFFFF, display_text);
+	mlx_string_put(game->mlx, game->win, 10, 20, 0xB8956D, display_text);
 	free(moves_str);
 	free(display_text);
 }
